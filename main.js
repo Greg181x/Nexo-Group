10a4151d6d3ff39ac74d2065361b2e140e665177
NexoGroup <tva2010.258@gmail.com>
2026-01-06 21:33:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -36,4 +36,8 @@ form.addEventListener("submit", async function (e) {
     navlinks.classList.toggle("active");
   });
 
-
+const btn = document.getElementById('backToTop');
+window.addEventListener('scroll', () => {
+  btn.style.display = window.scrollY > 300 ? 'block' : 'none';
+});
+btn.addEventListener('click', () => window.scrollTo({top:0, behavior:'smooth'}));
